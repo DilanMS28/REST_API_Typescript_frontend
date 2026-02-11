78cc0d565802b15b4a4f53c8009820970332ca80
Dilan <dilantienda90@gmail.com>
2026-02-11 16:40:01 -0600

    Funcion: Agrega el llamado a la funcion de loader de react router para procesar la vista de los productos

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Products from './views/Products';
+import {loader as productsLoader} from './views/Products';
 import NewProduct, {action as newProductAction} from './views/NewProduct';
 
 export const router = createBrowserRouter([
@@ -8,7 +9,7 @@ export const router = createBrowserRouter([
         path: "/", 
         element: <Layout />, 
         children: [
-            {element: <Products />, index: true}, //pagina principal
+            {element: <Products />, index: true, loader: productsLoader}, //pagina principal
             {path: "productos/nuevo", element: <NewProduct />, action: newProductAction}//agregar nuevo producto
         ] 
     }
