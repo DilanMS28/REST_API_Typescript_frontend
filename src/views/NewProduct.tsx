a41d0e1280abdc388667bc956728ad5aa72d77bb
Dilan <dilantienda90@gmail.com>
2026-02-11 13:23:32 -0600

    View: Agrega el formulario para insertar un producto junto con el action para procesar el formulario y los errores

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,12 +1,67 @@
-import { Link } from "react-router-dom";
+import { Link, Form, useActionData, type ActionFunctionArgs, redirect } from "react-router-dom";
+import ErrorMesagge from "../components/ErrorMessage";
+import { addProduct } from "../services/ProductService";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const data = Object.fromEntries(await request.formData());
+
+  let error = "";
+  if (Object.values(data).includes("")) {
+    error = "Todos los campos son obligatorios";
+  }
+  if (error.length) {//si hay error, se retorn el error para mostrarlo en la vista
+    return error;
+  }
+
+  await addProduct(data);
+
+  return redirect("/");//redirección a la vista principal;
+}
 
 export default function NewProduct() {
+
+  const error = useActionData() as string;
+
   return (
     <>
       <div className="flex justify-between">
         <h2 className="text-4xl font-black text-slate-500">Registrar Producto</h2>
         <Link to={"/"} className="rounded-md p-3 bg-indigo-600 text-sm font-bold text-white shadow-sm hover:bg-indigo-500">Volver a Productos</Link>
       </div>
+
+      {error && <ErrorMesagge>{error}</ErrorMesagge>}
+
+      <Form className="mt-10" method="POST">
+
+        <div className="mb-4">
+          <label htmlFor="name" className="text-gray-800">Nombre Producto:</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Nombre del Producto"
+            name="name"
+            className="mt-2 block w-full p-3 bg-gray-50"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">Precio:</label>
+          <input
+            id="price"
+            type="number"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+            className="mt-2 block w-full p-3 bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Registrar Producto"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+        />
+
+      </Form>
     </>
-  )
+  );
 }
