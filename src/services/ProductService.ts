7d32540672597c719b1f9c9f794235e15520bc04
Dilan <dilantienda90@gmail.com>
2026-02-12 14:12:35 -0600

    Funcion: Agrega la función de actualizar un producto con la api

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,4 +1,4 @@
-import { safeParse } from "valibot";
+import { number, safeParse, pipe, transform, string, parse } from "valibot";
 import axios from "axios";
 import { DraftProductSchema, ProductSchema, ProductsSchema, type Product } from "../types";
 
@@ -59,4 +59,26 @@ export async function getProductById(id: Product["id"]) {
   } catch (error) {
     console.log(error);
   }
+}
+export async function updateProduct(id: Product["id"], data: ProductData){
+  try {
+    
+    const NumberSchema =  pipe(string(), transform(Number), number())//esquema para convertir el price a number con vlaibot
+    
+    const result = safeParse(ProductSchema, {
+      id: id,
+      name: data.name,
+      price: parse(NumberSchema, data.price),
+      availability: data.availability === "true" ? true : false
+    })
+
+    if(result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+      await axios.put(url, result.output);
+    }
+    
+  } catch (error) {
+    console.log(error);
+  }
+
 }
