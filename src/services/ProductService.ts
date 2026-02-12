5129333d9208ff7397b83a4b71c9c3834d41b020
Dilan <dilantienda90@gmail.com>
2026-02-12 12:57:38 -0600

    Funcion: Agrega la funcion para obtener un producto por el Id desde el backend

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,6 @@
 import { safeParse } from "valibot";
 import axios from "axios";
-import { DraftProductSchema, ProductsSchema } from "../types";
+import { DraftProductSchema, ProductSchema, ProductsSchema, type Product } from "../types";
 
 type ProductData = {
   [k: string]: FormDataEntryValue;
@@ -35,6 +35,21 @@ export async function getProducts() {
     const {data} = await axios.get(url);
     const result = safeParse(ProductsSchema, data.data);
 
+    if(result.success) {
+      return result.output
+    }else{
+      throw new Error("Hubo un error...");
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getProductById(id: Product["id"]) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const {data} = await axios.get(url);
+    const result = safeParse(ProductSchema, data.data);
     if(result.success) {
       return result.output
     }else{
